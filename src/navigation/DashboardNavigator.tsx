2f838499948e75f523ac1caf9ac1e89a11044ce3
Abhishek Jha <abhishek.jha@wolfmatrix.com>
2024-12-02 23:14:09 +0545

    type safety in tab navigator

--- src/navigation/DashboardNavigator.tsx
+++ src/navigation/DashboardNavigator.tsx
@@ -1,14 +1,33 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import DashboardScreen from '../screens/DashboardScreen';
 import Icon from '@react-native-vector-icons/fontawesome'
+import { TouchableOpacity } from 'react-native';
 
 const Tab = createBottomTabNavigator()
 
-const MyTabs=()=>{
-    return(
-        <Tab.Navigator screenOptions={{headerShown:false}}>
-            <Tab.Screen name='Dashboard' component={DashboardScreen} options={{tabBarIcon:()=><Icon name='home' size={30}/>, tabBarShowLabel:false}}/>
-        </Tab.Navigator>
-    )
-}
-export default MyTabs
+const MyTabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const icons: Record<string, any> = {
+            Dashboard: 'home',
+          };
+          return (
+            <Icon
+              name={icons[route.name]}
+              color={color}
+              size={size}
+            />
+          );
+        },
+        tabBarShowLabel: false,
+        headerShown:false
+      })}
+    >
+      <Tab.Screen name="Dashboard" component={DashboardScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default MyTabs;
