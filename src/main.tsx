811f01ad440efde01b384d83c6161f7acd66e66d
Abhishek Jha <abhishek.jha@wolfmatrix.com>
2024-12-02 22:30:32 +0545

    configure react native vector icon and navigation

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Main: React.FC=()=>{
+    return(
+        <View>
+            <Text>Welcome to Smart Store Hub</Text>
+        </View>
+    )
+}
+
+export default Main;
