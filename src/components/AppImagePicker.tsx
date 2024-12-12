a635343088b30b8601f8aa3658c7a9c4ce16eaf3
Abhishek Jha <abhishek.jha@wolfmatrix.com>
2024-12-12 17:46:02 +0545

    Add image picker

--- src/components/AppImagePicker.tsx
+++ src/components/AppImagePicker.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import Icon from '@react-native-vector-icons/fontawesome'
+import Icon from '@react-native-vector-icons/fontawesome';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 
-const AppImagePicker:React.FC<{onPress:()=>void}> = ({onPress})=>{
+const AppImagePicker:React.FC= ()=>{
+    const handleImagePicker = async()=>{
+        const result = await launchImageLibrary({
+            mediaType:'photo'
+        })
+        if(result.didCancel || result.errorCode || result.errorMessage){
+            return;
+        }
+        if(result.assets){
+          console.log(result.assets)
+        }
+    }
     return(
-        <TouchableOpacity onPress={onPress} activeOpacity={0.8} style={{height:70, backgroundColor:'#FFF', borderRadius:10, justifyContent:'center', alignItems:'center'}}>
+        <TouchableOpacity onPress={handleImagePicker} activeOpacity={0.8} style={{height:70, backgroundColor:'#FFF', borderRadius:10, justifyContent:'center', alignItems:'center'}}>
             <View style={{justifyContent:'center', alignItems:'center'}}>
             <Icon name='cloud-upload' size={35}/>
              <Text>Upload the Picture</Text>
